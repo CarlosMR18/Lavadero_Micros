1fbd623f87917f95d300f393302d211608407d9c
inesfergil <167860686+inesfergil@users.noreply.github.com>
2024-05-11 20:49:56 +0200

    Update Parte_2.h

--- Parte_2.h
+++ Parte_2.h
@@ -4,10 +4,15 @@
 #define PARTE_2_H_
 
 --Incluir macros necesarias
---void funciones();
+//void funciones();
 void setup_luz(); 
-void luz(); 
+void control_L1(); 
 void setup_barrera(); 
 void barrera(); 
+void setup_lv();
+void lavadoV_on(); 
+void lavadoV_off();
+void setup_Parte_2(); 
+void Parte_2();  
 
 #endif /* PARTE_2_H_ */
