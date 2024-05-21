2960d4fbf3883f353a16d5a45d2cbccb64c442f7
nachobasterra01 <168533548+nachobasterra01@users.noreply.github.com>
2024-05-21 15:45:41 +0200

    Update Parte_1.h

--- Parte_1.h
+++ Parte_1.h
@@ -3,6 +3,28 @@
 #ifndef PARTE_1_H_
 #define PARTE_1_H_
 
+volatile uint32_t ms = 0;
+
+// Variables globales - Lavadero Horizontal
+volatile uint8_t so3 = 1;
+volatile uint8_t so4 = 1;
+volatile uint8_t so5 = 1;
+volatile uint8_t so3p = 1;
+volatile uint8_t so4p = 1;
+volatile uint8_t so5p = 1;
+volatile uint8_t aux_lavH = 0;
+volatile uint8_t limit_switch_lavH = 0; //'1' SW2 detecta rodillo en un extremo
+
+// Variables globales - Secado
+volatile uint8_t so7 = 1;
+volatile uint8_t so8 = 1;
+volatile uint8_t so9 = 1;
+volatile uint8_t so7p = 1;
+volatile uint8_t so8p = 1;
+volatile uint8_t so9p = 1;
+volatile uint8_t aux_sec = 0;
+volatile uint8_t limit_switch_sec = 0;        //'1' SW3 detecta rodillo en un extremo
+
 // Lavadero Horizontal
 void setup_LavHorizontal(void);
 
