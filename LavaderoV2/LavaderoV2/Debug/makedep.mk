c8132693e8ceb89ed2c08ac2f58789b708319303
leyrec12 <152520909+leyrec12@users.noreply.github.com>
2024-05-23 15:37:04 +0200

    Add files via upload

---
+++ LavaderoV2/LavaderoV2/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+General.c
+
+Integration.c
+
+main.c
+
+Parte_1.c
+
+Parte_2.c
+
+Parte_3.c
+
