f343e10ea119fb3d4791df601e2ab86b8b79ff79
leyrec12 <152520909+leyrec12@users.noreply.github.com>
2024-05-23 14:24:58 +0200

    Add files via upload

---
+++ LavaderoV2/LavaderoV2/main.c
@@ -0,0 +1,26 @@
+/*
+ * LavaderoV2.c
+ *
+ * Created: 22/05/2024 16:51:05
+ * Author : Admin
+ */ 
+
+#include "Integration.h"
+
+
+int main(void) {
+
+	setup_General();
+	setup_Integration();
+	setup_Parte1();
+	setup_Parte2();
+	setup_Parte3();
+	
+
+	while(1) {
+		Parte_1();
+		Parte_2();
+		Parte_3();
+	}
+	
+}
